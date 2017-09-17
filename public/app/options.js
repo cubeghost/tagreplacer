b4bf270e910bc571b3218223328450095b41c676
alex baldwin <alex@bldwn.co>
2017-09-17 18:02:04 -0400

    better looking options

--- public/app/options.js
+++ public/app/options.js
@@ -1,6 +1,14 @@
 var React = require('react');
 var createReactClass = require('create-react-class');
 
+// google material icon 'tune'
+const optionsIcon = (<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <title>Options</title>
+  <path d="M0 0h24v24H0z" fill="none"/>
+  <path d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z"/>
+</svg>);
+
+
 var Options = createReactClass({
 
   getInitialState: function() {
@@ -18,8 +26,7 @@ var Options = createReactClass({
   renderField: function(field, label) {
     var id = 'options-' + field;
 
-    return (<div>
-      <label htmlFor={id}>{label}</label>
+    return (<div className="field checkboxField">
       <input
         type="checkbox"
         checked={!!this.props.options[field]}
@@ -27,13 +34,16 @@ var Options = createReactClass({
         data-field={field}
         onChange={this.props.handleOptions}
       />
+      <label htmlFor={id}>{label}</label>
     </div>);
   },
 
   render: function() {
+    var buttonTitle = this.state.open ? 'Hide options' : 'Show options';
+
     return (<div className="options">
-      <button onClick={this.toggleOptions} className="toggleOptions">
-        options
+      <button title={buttonTitle} onClick={this.toggleOptions} className="toggleOptions" ref="toggleOptions">
+        {optionsIcon}
       </button>
       {this.state.open && (<div className="optionsForm">
         {this.renderField('includeQueue', 'Include queued posts')}
